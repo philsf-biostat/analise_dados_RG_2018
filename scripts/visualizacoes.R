ca760a9716b67cc94cbc4be44bcbc6dd11c774e8
Felipe Figueiredo <phisf79@gmail.com>
2018-06-24 01:35:14 -0300

    visualizações básicas - esportes principais predominantes, e secundários dos 2 principais

--- scripts/visualizacoes.R
+++ scripts/visualizacoes.R
@@ -1,1 +1,37 @@
 source('scripts/input.R')
+
+# ggplot(esportes[PRINCIPAL==1], aes(reorder(ESPORTE, ESPORTE, function(x) length(x)))) +
+ggplot(esportes[PRINCIPAL==1], aes(reorder(ESPORTE, ESPORTE, length))) +
+  geom_bar() +
+  xlab("Esporte") +
+  coord_flip() +
+  ggtitle("Esporte principal")
+
+ggplot(esportes[PRINCIPAL==0], aes(reorder(ESPORTE, ESPORTE, length ))) +
+  geom_bar() +
+  xlab("Esporte") +
+  coord_flip() +
+  ggtitle("Esporte secundário")
+
+# ggplot(participantes[`ESPORTE 1` == "FUTEBOL" & !is.na(`ESPORTE 2`)], aes(reorder(`ESPORTE 2`, `ESPORTE 2`, length))) +
+#   geom_bar() +
+#   xlab("Esporte") +
+#   coord_flip()+
+#   ggtitle("Esporte acessório quando o principal é futebol")
+
+ids.futebol <- esportes[PRINCIPAL == 1 & ESPORTE == "FUTEBOL"]$ID
+ids.futebol <- as.numeric(levels(ids.futebol)[ids.futebol])
+ids.corrida <- esportes[PRINCIPAL == 1 & ESPORTE == "CORRIDA"]$ID
+ids.corrida <- as.numeric(levels(ids.corrida)[ids.corrida])
+
+ggplot(esportes[ID %in% ids.futebol & PRINCIPAL == 0], aes(reorder(ESPORTE, ESPORTE, length))) +
+  geom_bar() +
+  xlab("Esporte") +
+  coord_flip()+
+  ggtitle("Esporte acessório quando o principal é futebol")
+
+ggplot(esportes[ID %in% ids.corrida & PRINCIPAL == 0], aes(reorder(ESPORTE, ESPORTE, length))) +
+  geom_bar() +
+  xlab("Esporte") +
+  coord_flip()+
+  ggtitle("Esporte acessório quando o principal é corrida")
