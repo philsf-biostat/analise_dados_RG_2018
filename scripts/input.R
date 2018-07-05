cdb495595e3798bea6803e194979a3abea55e20b
Felipe Figueiredo <phisf79@gmail.com>
2018-07-05 00:17:14 -0300

    dados recebidos (2018-07-04)

--- scripts/input.R
+++ scripts/input.R
@@ -4,8 +4,17 @@ library(readxl)
 participantes <- data.table(read_excel("dataset/dados padronizados.xls", "Participantes"))
 esportes <- data.table(read_excel("dataset/dados padronizados.xls", "Esportes"))
 esportes <- esportes[, lapply(.SD, factor)]
+dor <- data.table(read_excel("dataset/dados padronizados.xls", "Sente a dor"))
+dor <- dor[, lapply(.SD, factor)]
+movim <- data.table(read_excel("dataset/dados padronizados.xls", "Movimentos"))
+movim <- movim[, lapply(.SD, factor)]
+locais <- data.table(read_excel("dataset/dados padronizados.xls", "Locais de dor"))
+locais <- locais[, lapply(.SD, factor)]
 
 
 # remover participante sem dor
 participantes <- participantes[ID != 166]
 esportes <- esportes[ID != 166]
+movim <- movim[ID != 166]
+dor <- dor[ID != 166]
+locais <- locais[ID != 166]
