e77616af3b575c37ccc9552be584202826ff302d
Felipe Figueiredo <phisf79@gmail.com>
2018-07-05 00:07:45 -0300

    participante 166 não adere ao crit. inclus.

--- scripts/input.R
+++ scripts/input.R
@@ -4,3 +4,8 @@ library(readxl)
 participantes <- data.table(read_excel("dataset/dados padronizados.xls", "Participantes"))
 esportes <- data.table(read_excel("dataset/dados padronizados.xls", "Esportes"))
 esportes <- esportes[, lapply(.SD, factor)]
+
+
+# remover participante sem dor
+participantes <- participantes[ID != 166]
+esportes <- esportes[ID != 166]
