f6f637c1a70538a71559f25b842ef3ad074672a5
Felipe Figueiredo <philsf79@gmail.com>
2018-10-08 05:51:28 -0300

    remover ylab padrão "count"

--- scripts/visualizacoes.R
+++ scripts/visualizacoes.R
@@ -4,14 +4,14 @@ library(ggplot2)
 # ggplot(esportes[PRINCIPAL==1], aes(reorder(ESPORTE, ESPORTE, function(x) length(x)))) +
 esp.princ <- ggplot(esportes[PRINCIPAL==1], aes(reorder(ESPORTE, ESPORTE, length))) +
   geom_bar() +
-  xlab("Esporte") +
+  xlab("Esporte") + ylab("") +
   ylim(c(0, 150)) +
   coord_flip() +
   ggtitle("Esporte principal")
 
 esp.sec <- ggplot(esportes[PRINCIPAL==0], aes(reorder(ESPORTE, ESPORTE, length ))) +
   geom_bar() +
-  xlab("Esporte") +
+  xlab("Esporte") + ylab("") +
   ylim(c(0, 150)) +
   coord_flip() +
   ggtitle("Esporte secundário")
@@ -29,14 +29,14 @@ ids.corrida <- as.numeric(levels(ids.corrida)[ids.corrida])
 
 esp.sec.fut <- ggplot(esportes[ID %in% ids.futebol & PRINCIPAL == 0], aes(reorder(ESPORTE, ESPORTE, length))) +
   geom_bar() +
-  xlab("Esporte") +
+  xlab("Esporte") + ylab("") +
   ylim(c(0, 40)) +
   coord_flip()+
   ggtitle("Esporte acessório quando o principal é futebol")
 
 esp.sec.corr <- ggplot(esportes[ID %in% ids.corrida & PRINCIPAL == 0], aes(reorder(ESPORTE, ESPORTE, length))) +
   geom_bar() +
-  xlab("Esporte") +
+  xlab("Esporte") + ylab("") +
   ylim(c(0, 40)) +
   coord_flip()+
   ggtitle("Esporte acessório quando o principal é corrida")
