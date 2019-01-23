11ba1e70e2160562ef54cae16ae55dda0ca3dfe0
Felipe Figueiredo <philsf79@gmail.com>
2019-01-23 13:03:07 -0200

    tabela de resultados CIRURGIA x ESPORTE

--- scripts/res-assoc-esp-cirurg.R
+++ scripts/res-assoc-esp-cirurg.R
@@ -5,3 +5,8 @@ esportes.cirurg[, PRINCIPAL := NULL]
 esportes.cirurg$ESPORTE <- forcats::fct_other(esportes.cirurg$ESPORTE, keep = c("FUTEBOL", "CORRIDA", "MUSCULAÇÃO"), other_level = "OUTROS")
 summary(esportes.cirurg)
 
+# with(esportes.cirurg, table(ESPORTE, CIRURGIA))
+# fisher.test(esportes.cirurg$ESPORTE, esportes.cirurg$CIRURGIA)
+# chisq.test(esportes.cirurg$ESPORTE, esportes.cirurg$CIRURGIA)
+
+tab.esportes.cirurg <- CreateTableOne("CIRURGIA", "ESPORTE", esportes.cirurg)
