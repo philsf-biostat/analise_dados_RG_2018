3fd445804eb714cf3afa71f8efbaa05199546e49
Felipe Figueiredo <philsf79@gmail.com>
2019-01-24 15:22:23 -0200

    Ordenar os níveis do ESPORTE em ordem decrescente (`forcats` rulez!)

--- scripts/res-assoc-esp-cirurg.R
+++ scripts/res-assoc-esp-cirurg.R
@@ -3,6 +3,7 @@ library(tableone)
 
 esportes.cirurg <- esportes[participantes[, .(ID, CIRURGIA)], , on = .(ID)]
 esportes.cirurg[, PRINCIPAL := NULL]
+esportes.cirurg$ESPORTE <- forcats::fct_infreq(esportes.cirurg$ESPORTE)
 esportes.cirurg$ESPORTE <- forcats::fct_other(esportes.cirurg$ESPORTE, keep = c("FUTEBOL", "CORRIDA", "MUSCULAÇÃO"), other_level = "OUTROS")
 summary(esportes.cirurg)
 
--- scripts/res-assoc-esp-movim.R
+++ scripts/res-assoc-esp-movim.R
@@ -3,6 +3,7 @@ library(tableone)
 
 esportes.movim <- movim[esportes[PRINCIPAL=="Principal"], , on = .(ID)]
 esportes.movim[, PRINCIPAL := NULL]
+esportes.movim$ESPORTE <- forcats::fct_infreq(esportes.movim$ESPORTE)
 esportes.movim$ESPORTE <- forcats::fct_other(esportes.movim$ESPORTE, keep = c("FUTEBOL", "CORRIDA", "MUSCULAÇÃO"), other_level = "OUTROS")
 summary(esportes.movim)
 
