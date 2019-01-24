2a7119fbfe15916494e4378b552a28e316ca1125
Felipe Figueiredo <philsf79@gmail.com>
2019-01-24 14:47:22 -0200

    tabela de resultados MOVIMENTO x ESPORTE

--- scripts/res-assoc-esp-movim.R
+++ scripts/res-assoc-esp-movim.R
@@ -5,3 +5,9 @@ esportes.movim <- movim[esportes[PRINCIPAL=="Principal"], , on = .(ID)]
 esportes.movim[, PRINCIPAL := NULL]
 esportes.movim$ESPORTE <- forcats::fct_other(esportes.movim$ESPORTE, keep = c("FUTEBOL", "CORRIDA", "MUSCULAÇÃO"), other_level = "OUTROS")
 summary(esportes.movim)
+
+# with(esportes.movim, table(MOVIMENTO, ESPORTE))
+# fisher.test(esportes.movim$ESPORTE, esportes.movim$MOVIMENTO)
+# chisq.test(esportes.movim$ESPORTE, esportes.movim$MOVIMENTO)
+
+tab.esportes.movim <- CreateTableOne("MOVIMENTO", "ESPORTE", esportes.movim)
