833f6214454835825166038c94312c1458f5db1b
Felipe Figueiredo <philsf79@gmail.com>
2019-01-25 15:14:30 -0200

    usar ef.colnames

--- scripts/resultados.R
+++ scripts/resultados.R
@@ -7,7 +7,7 @@ source('scripts/res-assoc-tempo-EF.R', encoding = 'UTF-8')
 source('scripts/res-assoc-tempo-idade.R', encoding = 'UTF-8')
 source('scripts/res-assoc-tempo-nivel.R', encoding = 'UTF-8')
 
-tab.ef.t1 <- CreateTableOne(c("EF1", "EF2", "EF3", "EF4", "EF5", "EF6", "EF7", "EF8", "EF9", "EF10"), data = participantes)
+tab.ef.t1 <- CreateTableOne(ef.colnames, data = participantes)
 tab.ef <- print(tab.ef.t1, printToggle = FALSE)
 
 tab.esportes <- rbind(
