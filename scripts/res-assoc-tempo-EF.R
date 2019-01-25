7071c81cd0aa1e3797c1054f8fa009d4c3ba4dce
Felipe Figueiredo <philsf79@gmail.com>
2019-01-25 16:40:15 -0200

    cleanup

--- scripts/res-assoc-tempo-EF.R
+++ scripts/res-assoc-tempo-EF.R
@@ -1,25 +1,22 @@
 source('scripts/input.R', encoding = 'UTF-8')
 library(tableone)
 
-# plot(participantes[, .(EF1, TEMPO)])
-# hist(participantes$TEMPO)
-# shapiro.test(participantes$TEMPO)
+tab.tempo.ef <- CreateTableOne(ef.colnames, "TEMPO.cat", participantes)
 
-tab.tempo.ef <- CreateTableOne(ef.colnames, "TEMPO.ano", participantes)
-tab.tempo <- print(tab.tempo.ef, exact = TRUE, printToggle = FALSE)
+# obsoleto ----------------------------------------------------------------
 
-tab.tempo.ef <- rbind(
-  print(CreateTableOne("TEMPO", "EF1", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF2", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF3", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF4", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF5", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF6", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF7", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF8", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF9", participantes), nonnormal = TRUE, printToggle = FALSE),
-  print(CreateTableOne("TEMPO", "EF10", participantes), nonnormal = TRUE, printToggle = FALSE)
-)
+# tab.tempo.ef.num <- rbind(
+#   print(CreateTableOne("TEMPO", "EF1", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF2", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF3", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF4", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF5", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF6", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF7", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF8", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF9", participantes), nonnormal = TRUE, printToggle = FALSE),
+#   print(CreateTableOne("TEMPO", "EF10", participantes), nonnormal = TRUE, printToggle = FALSE)
+# )
 
 # participantes[, .(p = wilcox.test(TEMPO ~ EF1)$p.value), by = ef]
 # wilcox.test(TEMPO ~ EF1, participantes)
--- scripts/resultados.R
+++ scripts/resultados.R
@@ -15,13 +15,9 @@ tab.esportes <- rbind(
   print(tab.esportes.movim, exact = FALSE, printToggle = FALSE)
 )
 
-# # Tempo numérico (dias)
 tab.tempo <- rbind(
-  tab.tempo.ef
+  print(tab.tempo.ef, exact = TRUE, printToggle = FALSE)
 )
-
-## Categorizado (corte = 1 ano)
-## ocultar código acima (tab.tempo gerada no protótipo)
 
 # Pós-processamento das tabelas
 rownames(tab.ef) <- str_replace(rownames(tab.ef), '= 1 \\(%\\)', '(%)')
