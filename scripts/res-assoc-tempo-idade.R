2d40899b1c16fb72961162993debab9b430819fe
Felipe Figueiredo <philsf79@gmail.com>
2019-01-28 16:13:52 -0200

    scripts de associação removidos do repo

--- scripts/res-assoc-tempo-idade.R
+++
@@ -1,6 +0,0 @@
-source('scripts/input.R', encoding = 'UTF-8')
-library(tableone)
-
-# with(participantes, cor.test(TEMPO, IDADE, method = "spearman"))
-
-tab.tempo.idade <- CreateTableOne("IDADE.cat", "TEMPO.cat", participantes)
--- scripts/res-assoc-tempo-nivel.R
+++
@@ -1,4 +0,0 @@
-source('scripts/input.R', encoding = 'UTF-8')
-library(tableone)
-
-tab.tempo.nivel <- CreateTableOne("NIVEL", "TEMPO.cat", participantes)
