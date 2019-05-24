e77820b3bc608e8a2559588d2438ac864bd5308c
Felipe Figueiredo <philsf79@gmail.com>
2019-05-24 13:52:40 -0300

    remover level das rownames da tabela

--- scripts/resultados.R
+++ scripts/resultados.R
@@ -1,6 +1,7 @@
 source('scripts/input.R', encoding = 'UTF-8')
 library(tableone)
+library(stringr)
 
 tab.ef.t1 <- CreateTableOne(ef.colnames, data = participantes)
 tab.ef <- print(tab.ef.t1, printToggle = FALSE)
-
+rownames(tab.ef) <- str_remove(rownames(tab.ef), " = 1")
